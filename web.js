4d0d45add19b24e004f4d9634fb82dd4ddd6861c
Ben Johnson <code@benrobjoh.com>
2013-07-06 21:25:05 +0000

    Homework 3: Part 1, Changed web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var buffer = fs.readFileSync("index.html");
+  var string = buffer.toString();
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
